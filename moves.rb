5e3fcfff5e70080e2fc3215601622757a7c3c860
Sam Auciello <olleicua@gmail.com>
2014-02-26 11:29:17 -0500

    refactoring and working on solver

--- moves.rb
+++ moves.rb
@@ -1,22 +1,50 @@
 require 'game'
 
-b = Game.new
-b.print
-moves = []
-
-while true
-  print '> '
-  STDOUT.flush
-  command = gets.chomp
-  case command
-  when 'moves'
-    b.moves.each do |m|
-      b.after_move(m) do
-        puts "#{m}: #{b.moves.count}"
+class UI
+  def initialize
+    @b = Game.new
+    @b.print
+    @moves = []
+  end
+  
+  def run
+    while true
+      print '> '
+      STDOUT.flush
+      command = gets.chomp
+      command.split(';').each do |cmd|
+        case cmd
+        when 'moves' then moves
+        when /^a ?([wb]) ?([rnbqkp]) ?([a-h][1-8])$/i
+          add $1, $2, $3
+        when /^r ?([a-h][1-8])$/
+          @b.pop($1)
+        when /^m ?([a-h][1-8]) ?([a-h][1-8])$/
+          move $1, $2
+        when 'u' then unmove
+        when 's' then solve.each{|m| puts m}
+        when 't' then @b.toggle_turn
+        when 'd' then binding.pry
+        when 'quit' then exit
+        else next
+        end
       end
+      @b.print
     end
-  when /^a ?([wb]) ?([rnbqkp]) ?([a-h][1-8])$/i
-    klass = case $2
+  end
+  
+  def count_after m
+    @b.after_move(m){@b.moves.size}
+  end
+  
+  def moves
+    @b.moves.each do |m|
+      puts "#{m}: #{count_after m}"
+    end
+  end
+  
+  def add color, piece, space
+    klass = case piece
             when 'r' then Rook
             when 'n' then Knight
             when 'b' then Bishop
@@ -24,30 +52,41 @@ while true
             when 'k' then King
             when 'p' then Pawn
             end
-    color = if $1 == 'w' then :white else :black end
-    b[$3]= klass.new(color)
-    b.pieces << b[$3]
-  when /^r ?([a-h][1-8])$/
-    b.pop($1)
-  when /^m ?([a-h][1-8]) ?([a-h][1-8])$/
-    if b[$1]
-      m = Move.new $1, $2, b[$1]
-      b.move m
-      moves << m
-      if b.moves.count == 1
-        m = b.moves.first
-        b.move m
-        moves << m
+    color = if color == 'w' then :white else :black end
+    @b[space]= klass.new(color)
+    @b.pieces << @b[space]
+  end
+  
+  def move start, _end
+    if @b[start]
+      m = Move.new start, _end, @b[start]
+      @b.move m
+      @moves << m
+      if @b.moves.count == 1
+        m = @b.moves.first
+        @b.move m
+        @moves << m
       end
     end
-  when 'u'
-    b.unmove moves.pop unless moves.empty
-  when 't'
-    b.toggle_turn
-  when 'd'
-    binding.pry
-  when 'quit' then break
-  else next
   end
-  b.print
+  
+  def unmove
+    @b.unmove @moves.pop unless @moves.empty?
+  end
+  
+  def solve
+    (@b.winning? or
+     @b.moves.select do |m|
+       (count_after(m).zero? or
+        (count_after(m) == 1 and
+         begin
+           move m.start, m.end
+           ret = @moves.dup if solve
+           unmove
+           ret
+         end))
+     end)
+  end
 end
+
+UI.new.run
